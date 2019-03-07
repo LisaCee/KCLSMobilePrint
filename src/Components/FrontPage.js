de9201fc5fd855693a61e69c393107ed68dc9b19
LisaCee <lisaceedesign@gmail.com>
2019-03-06 21:30:56 -0800

    changed organization structure

---
+++ src/Components/FrontPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LibraryDD from "./LibraryDD";
+import Instructions from "./Instructions";
+
+const FrontPage = () => {
+  return (
+    <div>
+      <Instructions />
+      <LibraryDD />
+    </div>
+  );
+};
+
+export default FrontPage;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import FrontPage from "./Components/FrontPage";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App() {
+    return (
+        <div className="App">
+            <h1>KCLS Mobile Printing</h1>
+            <FrontPage />
+        </div>
+    );
+}
 
-
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
