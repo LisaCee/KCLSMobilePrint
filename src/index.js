8c03394448da392b91f4f453a81eb7043136c832
LisaCee <lisaceedesign@gmail.com>
2019-03-08 14:45:25 -0800

    reactstrap

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import FrontPage from "./Components/FrontPage";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {Container, Row, Col} from 'reactstrap';
 
 function App() {
     return (
-        <div className="App">
-            <h1>KCLS Mobile Printing</h1>
-            <FrontPage />
-        </div>
+      <div className="App">
+        <Container>
+          <h1>KCLS Mobile Printing</h1>
+          <FrontPage />
+        </Container>
+      </div>
     );
 }
 
